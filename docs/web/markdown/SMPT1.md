4ad3b76f679e89887a9976ad1a0f36d5c9866214
Dante Gallardo <159446379+dantegld@users.noreply.github.com>
2024-12-04 00:42:31 +0100

    Delete docs directory

--- docs/web/markdown/SMPT1.md
+++
@@ -1,5 +0,0 @@
-# Bronnen
-
-@mikikiv [Codecademy](https://www.codecademy.com/resources/docs/markdown)
-
-[GitLab](https://docs.gitlab.com/ee/user/markdown.html)
--- docs/web/responsve dropdown/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code for toggling the navigation menu
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav ul');
-
-hamburger.addEventListener('click', function() {
-    nav.classList.toggle('open');
-});
